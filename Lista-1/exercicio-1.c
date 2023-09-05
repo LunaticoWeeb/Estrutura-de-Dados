fb7925c55729f0169a28a1f70d672bc2e5a95830
LunaticoWeeb <2sa4vini@gmail.com>
2023-09-05 20:15:07 -0300

    Versão 0 exercicio-1

---
+++ Lista-1/exercicio-1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	char cadeia1[] = "Nome1";
+	char cadeia2[] = "Nome2";
+	char concatenador = '-';
+
+	printf("%s%c%s", cadeia1, concatenador, cadeia2);
+
+	return 0;
+}
