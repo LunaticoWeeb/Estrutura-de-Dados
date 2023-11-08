0126a75b06bd8c8b11f3066b14d67b412a7eedd8
LunaticoWeeb <2sa4vini@gmail.com>
2023-11-08 14:27:49 -0300

    Limpeza e organização de arquivos

--- Lista-4/menu-teste.c
+++ Lista-4/menu-teste.c
@@ -7,14 +7,6 @@ int main(void){
       int exec = 1;
       int e;
 
-      // Inserindo alguns itens para testes:
-      // setItem(list, "Zeus", &e);
-      // setItem(list, "Poseidon", &e);
-      // setItem(list, "Hades", &e);
-      // setItem(list, "Ares", &e);
-      // setItem(list, "Hefesto", &e);
-      // printLista(list);
-
       while (exec)
       {
             int operation = RecievesOperation();
