8ac086cac5d3461628b96f45abae15a6b9da9f0e
LunaticoWeeb <2sa4vini@gmail.com>
2023-11-08 13:24:39 -0300

    Teste futuro em comentário

--- Lista-4/main.c
+++ Lista-4/main.c
@@ -3,7 +3,17 @@
 int main(void){
 
       Lista *list = createLista();
+      if(list == NULL) return 1;
       int exec = 1;
+      int e;
+
+      // Inserindo alguns itens para testes:
+      // setItem(list, "Zeus", &e);
+      // setItem(list, "Poseidon", &e);
+      // setItem(list, "Hades", &e);
+      // setItem(list, "Ares", &e);
+      // setItem(list, "Hefesto", &e);
+      // printLista(list);
 
       while (exec)
       {
